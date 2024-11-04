569d3aaf9915886d25eaa516a6b8c55cfa83526a
Jiaqi Wen <127540860+wenjiaqi8255@users.noreply.github.com>
2024-11-04 22:11:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,48 @@
+function skillsMember() {
+  return [
+    {
+      name: 'HTML5',
+      level: 3
+    },
+    {
+      name: 'CSS3',
+      level: 3
+    },
+    {
+      name: 'JavaScript',
+      level: 3
+    },
+    {
+      name: 'React',
+      level: 2
+    },
+    {
+      name: 'Node.js',
+      level: 2
+    },
+    {
+      name: 'MongoDB',
+      level: 2
+    },
+    {
+      name: 'Express',
+      level: 2
+    },
+    {
+      name: 'Python',
+      level: 2
+    },
+    {
+      name: 'Django',
+      level: 2
+    },
+    {
+      name: 'GraphQL',
+      level: 1
+    },
+    {
+      name: 'Apollo Client',
+      level: 1
+    }
+  ]
+}
